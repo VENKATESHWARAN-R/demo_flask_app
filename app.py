b869bc0e1962994d43979f0bafcf71b0efc0aaa6
Venkateshwaran R <venkateshwaran633@gmail.com>
2022-12-06 20:04:44 +0200

    added new function in app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def hello_world():
     return 'Flask says Hello World '
 
 
+@app.route('/test')
+def test():
+    return 'This is just a test case'
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
